65fb2b8abeead94770166498d8043bcb5c0f6392
alifeee <alifeee.web@outlook.com>
2024-12-17 09:21:55 +0000

    formatting

--- qr.py
+++ qr.py
@@ -4,48 +4,47 @@ import argparse
 from escpos.printer import Usb
 from escpos.exceptions import ImageWidthError
 
+
 def main(
-	url: str,
-	title: str,
+    url: str,
+    title: str,
 ):
-	p = Usb(0x0416, 0x5011, profile="ZJ-5870")
+    p = Usb(0x0416, 0x5011, profile="ZJ-5870")
 
-	if title != "":
-		p.set(
-			align="center",
-			bold=True,
-			double_height=True,
-			double_width=True,
-		)
-		p.text(title)
+    if title != "":
+        p.set(
+            align="center",
+            bold=True,
+            double_height=True,
+            double_width=True,
+        )
+        p.text(title)
 
-	size = 16
-	while True:
-		try:
-			p.qr(
-				url,
-				size=size, # pixels
-				ec=1, # error correction
-				native=False, # do natively (doesn't work I think)
-				center=True,
-			)
-			break
-		except ImageWidthError as e:
-			size -= 1
-		if size == 1:
-			break
+    size = 16
+    while True:
+        try:
+            p.qr(
+                url,
+                size=size,  # pixels
+                ec=1,  # error correction
+                native=False,  # do natively (doesn't work I think)
+                center=True,
+            )
+            break
+        except ImageWidthError:
+            size -= 1
+        if size == 1:
+            break
 
-	p.set(normal_textsize=True)
-	p.textln(url)
-	p.ln(3)
-	p.close()
+    p.set(normal_textsize=True)
+    p.textln(url)
+    p.ln(3)
+    p.close()
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-url", "--url", help="url to qr", required=True, type=str)
-  parser.add_argument("-t", "--title", help="title of QR code", type=str)
-  args = parser.parse_args()
-  main(
-		args.url,
-		args.title
-	)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-url", "--url", help="url to qr", required=True, type=str)
+    parser.add_argument("-t", "--title", help="title of QR code", type=str)
+    args = parser.parse_args()
+    main(args.url, args.title)
